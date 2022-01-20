0090f0045d44bec1efb64b0eaa315f0dbdb81b2e
TI SATC <deptotisatc@gmail.com>
2022-01-20 02:55:25 -0300

    arrow function: deixando o codigo menos verboso

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -18,17 +18,17 @@ class NegociacaoController {
         let data = new Date(
             this._inputData.value
             .split("-")
-            .map((value, index)=>{
-                return value
-            })
+            .map((value, index)=>value)
         )
         console.log(data);
 
-        // let negociacao = new Negociacao(
-        //     this._inputData.value,
-        //     this._inputQuantidade.value,
-        //     this._inputValor.value
-        // );
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+
+        console.log(negociacao)
 
         // adicionar a negociação em uma lista
 
