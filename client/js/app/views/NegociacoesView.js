be6aaf7272248e8ba0200a1a70c4ac1300063893
TI SATC <deptotisatc@gmail.com>
2022-01-24 04:03:06 -0300

    feat: Totalizando o volume em nosso Template

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -21,19 +21,29 @@ class NegociacoesView {
                 </thead>
 
                 <tbody>
-                ${model.negociacoes.map(i=>{
-                    return `
+                ${model.negociacoes.map(i =>
+                    `
                         <tr>
                             <th>${DateHelper.dataParaTexto(i.data)}</th>
                             <th>${i.quantidade}</th>
                             <th>${i.valor}</th>
                             <th>${i.volume}</th>
                         </tr>
-                    `;
-                }).join()}
+                    `
+                ).join()}
                 </tbody>
             
                 <tfoot>
+                    <td colspan=3></td>
+                    <td>
+                        ${
+                            (function(){
+                                let total = 0;
+                                model.negociacoes.forEach(n =>total+=n.volume)
+                                return total;
+                            })()
+                        }
+                    </td>
                 </tfoot>
             </table>
         `
